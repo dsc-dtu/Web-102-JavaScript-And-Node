bc8fac9f828aef80942b1aa744982958f86311ca
saurabhmittal16 <saurabhmittal1606@gmail.com>
2019-11-09 21:10:59 +0530

    Use URL module in node

--- First Server Demo/index.js
+++ First Server Demo/index.js
@@ -6,7 +6,14 @@ const port = 5678;
 
 const server = http.createServer((req, res) => {
 	res.statusCode = 200;
-	res.setHeader("Content-Type", "text/plain");
+	res.writeHead(200, { "Content-Type": "text/plain" });
+
+	// Route visited
+	console.log(req.url);
+
+	// The query parameters provided in route
+	const q = url.parse(req.url, true).query;
+	console.log(q);
 
 	res.end("Hello World\n");
 });
